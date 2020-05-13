981094bbb8421d984e1a3973c668d6055e0227d5
jngcii <concotree@gmail.com>
2020-05-13 18:28:36 +0900

    Update reivews app

--- reviews/views.py
+++ reviews/views.py
@@ -42,7 +42,7 @@ def update_review(request, review_pk):
 
 # 리뷰 삭제
 @login_required
-def delete(request, review_pk):
+def delete_review(request, review_pk):
     review = get_object_or_404(Review, pk=review_pk)
     movie_pk = review.movie.id
     if review.reviewer == request.user:
@@ -62,3 +62,42 @@ def detail(request, review_pk):
     return render(request, 'review/detail.html', context)
 
 # 코멘트 생성
+@login_required
+def create_comment(request, review_pk):
+    review = get_object_or_404(Review, pk=review_pk)
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.commenter = request.user
+        comment.review = review
+        comment.save()
+        return redirect('reviews:detail', review_pk)
+    else:
+        messages.error(request, '댓글을 다시 입력해주세요.')
+    return redirect('reviews:detail', movie_pk)
+
+# 코멘트 수정
+@login_required
+@require_POST
+def update_comment(request, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    review_pk = comment.review.id
+    form = commentForm(request.POST, instace=comment)
+
+    if form.is_valid():
+        form.save()
+        return redirect('reviews:detail', review_pk)
+    else:
+        messages.error(request, '댓글을 다시 입력해주세요.')
+    return redirect('reviews:detail', review_pk)
+
+# 코멘트 삭제
+@login_required
+def delete_comment(request, comment_pk):
+    comment = get_object_or_404(Comment, pk=comment_pk)
+    review_pk = comment.review.id
+    if comment.commenter == request.user:
+        comment.delete()
+    else:
+        message.error(request, '삭제 권한이 없습니다.')
+    return redirect('reviews:detail', review_pk)
